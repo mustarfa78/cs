6dfc5af11f9904cbb2eb7509a05f636fab96f91d
mustarfa78 <mustarfa78@gmail.com>
2025-10-27 14:53:28 +1100

    Implement Stage 3 apple and passage mechanics

--- CSgame.c
+++ CSgame.c
@@ -15,6 +15,8 @@
 
 #define NO_SNAKE -1
 
+#define MAX_SNAKE_LENGTH (ROWS * COLS)
+
 // Add your own #define constants below this line
 
 
@@ -91,7 +93,17 @@ void print_current_statistics(
     int moves_made,
     int apples_eaten,
     int apples_remaining,
-    int apples_initial
+    int apples_initial,
+    int points_remaining_total
+);
+void reverse_snake(int snake_rows[MAX_SNAKE_LENGTH], int snake_cols[MAX_SNAKE_LENGTH],
+    int snake_length);
+void remove_tail_segments(
+    struct tile board[ROWS][COLS],
+    int snake_rows[MAX_SNAKE_LENGTH],
+    int snake_cols[MAX_SNAKE_LENGTH],
+    int *snake_length,
+    int segments_to_remove
 );
 
 
@@ -107,6 +119,10 @@ int main(void) {
     char command;
     int apples_initial = 0;
     int apples_remaining = 0;
+    int points_remaining_total = 0;
+    int snake_rows[MAX_SNAKE_LENGTH];
+    int snake_cols[MAX_SNAKE_LENGTH];
+    int snake_length = 0;
     while (scanf(" %c", &command) == 1) {
         if (command == 's' || command == 'S') {
             break;
@@ -171,7 +187,18 @@ int main(void) {
             if (scanf(" %c %d %d", &apple_type, &row, &col) != 3) {
                 continue;
             }
-            if (apple_type != 'n') {
+            enum entity apple_entity;
+            int apple_points = 0;
+            if (apple_type == 'n') {
+                apple_entity = APPLE_NORMAL;
+                apple_points = 5;
+            } else if (apple_type == 'r') {
+                apple_entity = APPLE_REVERSE;
+                apple_points = 10;
+            } else if (apple_type == 's') {
+                apple_entity = APPLE_SPLIT;
+                apple_points = 20;
+            } else {
                 continue;
             }
             if (!is_position_in_bounds(row, col)) {
@@ -190,9 +217,51 @@ int main(void) {
                 );
                 continue;
             }
-            board[row][col].entity = APPLE_NORMAL;
+            board[row][col].entity = apple_entity;
             apples_initial++;
             apples_remaining++;
+            points_remaining_total += apple_points;
+            continue;
+        }
+
+        if (command == 'p') {
+            char passage_direction;
+            int row;
+            int col;
+            if (scanf(" %c %d %d", &passage_direction, &row, &col) != 3) {
+                continue;
+            }
+            if (!is_position_in_bounds(row, col)) {
+                printf(
+                    "ERROR: Invalid position, %d %d is out of bounds!\n",
+                    row,
+                    col
+                );
+                continue;
+            }
+            if (!is_tile_empty(board, row, col)) {
+                printf(
+                    "ERROR: Invalid tile, %d %d is occupied!\n",
+                    row,
+                    col
+                );
+                continue;
+            }
+
+            enum entity passage_entity = EMPTY;
+            if (passage_direction == '^') {
+                passage_entity = PASSAGE_UP;
+            } else if (passage_direction == 'v') {
+                passage_entity = PASSAGE_DOWN;
+            } else if (passage_direction == '<') {
+                passage_entity = PASSAGE_LEFT;
+            } else if (passage_direction == '>') {
+                passage_entity = PASSAGE_RIGHT;
+            } else {
+                continue;
+            }
+
+            board[row][col].entity = passage_entity;
             continue;
         }
 
@@ -280,6 +349,9 @@ int main(void) {
     board[snake_row][snake_col].entity = BODY_SEGMENT;
     int snake_head_row = snake_row;
     int snake_head_col = snake_col;
+    snake_rows[0] = snake_row;
+    snake_cols[0] = snake_col;
+    snake_length = 1;
     int moves_made = 0;
     int apples_eaten = 0;
     int points = 0;
@@ -302,7 +374,8 @@ int main(void) {
                 moves_made,
                 apples_eaten,
                 apples_remaining,
-                apples_initial
+                apples_initial,
+                points_remaining_total
             );
             continue;
         }
@@ -328,42 +401,123 @@ int main(void) {
 
         int lose_game = 0;
         int win_game = 0;
+        int should_add_segment = 0;
+        int apple_value = 0;
+        int consumed_reverse = 0;
+        int consumed_split = 0;
 
         if (!is_position_in_bounds(new_row, new_col)) {
             lose_game = 1;
         } else {
             enum entity target_entity = board[new_row][new_col].entity;
-            if (
-                target_entity == WALL ||
-                target_entity == BODY_SEGMENT
-            ) {
+
+            if (target_entity == WALL || target_entity == BODY_SEGMENT) {
                 lose_game = 1;
-                snake_head_row = new_row;
-                snake_head_col = new_col;
             } else if (target_entity == EXIT_LOCKED) {
                 if (apples_remaining == 0) {
                     board[new_row][new_col].entity = EXIT_UNLOCKED;
-                    snake_head_row = new_row;
-                    snake_head_col = new_col;
                     win_game = 1;
                 } else {
                     lose_game = 1;
-                    snake_head_row = new_row;
-                    snake_head_col = new_col;
                 }
             } else if (target_entity == EXIT_UNLOCKED) {
-                snake_head_row = new_row;
-                snake_head_col = new_col;
                 win_game = 1;
+            } else if (
+                target_entity == PASSAGE_UP ||
+                target_entity == PASSAGE_DOWN ||
+                target_entity == PASSAGE_LEFT ||
+                target_entity == PASSAGE_RIGHT
+            ) {
+                int correct_direction = 0;
+                if (target_entity == PASSAGE_UP && delta_row == -1) {
+                    correct_direction = 1;
+                } else if (target_entity == PASSAGE_DOWN && delta_row == 1) {
+                    correct_direction = 1;
+                } else if (target_entity == PASSAGE_LEFT && delta_col == -1) {
+                    correct_direction = 1;
+                } else if (target_entity == PASSAGE_RIGHT && delta_col == 1) {
+                    correct_direction = 1;
+                }
+
+                if (correct_direction) {
+                    should_add_segment = 1;
+                    board[new_row][new_col].entity = BODY_SEGMENT;
+                } else {
+                    lose_game = 1;
+                }
             } else {
+                should_add_segment = 1;
+
                 if (target_entity == APPLE_NORMAL) {
+                    apple_value = 5;
                     apples_eaten++;
                     apples_remaining--;
-                    points += 5;
+                    points += apple_value;
+                    points_remaining_total -= apple_value;
+                } else if (target_entity == APPLE_REVERSE) {
+                    apple_value = 10;
+                    apples_eaten++;
+                    apples_remaining--;
+                    points += apple_value;
+                    points_remaining_total -= apple_value;
+                    consumed_reverse = 1;
+                } else if (target_entity == APPLE_SPLIT) {
+                    apple_value = 20;
+                    apples_eaten++;
+                    apples_remaining--;
+                    points += apple_value;
+                    points_remaining_total -= apple_value;
+                    consumed_split = 1;
                 }
+
+                if (points_remaining_total < 0) {
+                    points_remaining_total = 0;
+                }
+
+                board[new_row][new_col].entity = BODY_SEGMENT;
+            }
+        }
+
+        if (should_add_segment) {
+            snake_rows[snake_length] = new_row;
+            snake_cols[snake_length] = new_col;
+            snake_length++;
+
+            snake_head_row = new_row;
+            snake_head_col = new_col;
+
+            if (consumed_reverse) {
+                reverse_snake(snake_rows, snake_cols, snake_length);
+                snake_head_row = snake_rows[snake_length - 1];
+                snake_head_col = snake_cols[snake_length - 1];
+            }
+
+            if (consumed_split) {
+                int body_segments = snake_length - 1;
+                int segments_to_remove = 0;
+                if (body_segments > 0) {
+                    if (body_segments % 2 == 0) {
+                        segments_to_remove = body_segments / 2;
+                    } else {
+                        segments_to_remove = (body_segments + 1) / 2;
+                    }
+                }
+                if (segments_to_remove > 0) {
+                    remove_tail_segments(
+                        board,
+                        snake_rows,
+                        snake_cols,
+                        &snake_length,
+                        segments_to_remove
+                    );
+                }
+                snake_head_row = snake_rows[snake_length - 1];
+                snake_head_col = snake_cols[snake_length - 1];
+            }
+        } else if (win_game || lose_game) {
+            if (is_position_in_bounds(new_row, new_col)) {
                 snake_head_row = new_row;
                 snake_head_col = new_col;
-                board[new_row][new_col].entity = BODY_SEGMENT;
             }
         }
 
@@ -381,7 +535,8 @@ int main(void) {
                 moves_made,
                 apples_eaten,
                 apples_remaining,
-                apples_initial
+                apples_initial,
+                points_remaining_total
             );
             return 0;
         }
@@ -394,7 +549,8 @@ int main(void) {
                 moves_made,
                 apples_eaten,
                 apples_remaining,
-                apples_initial
+                apples_initial,
+                points_remaining_total
             );
             return 0;
         }
@@ -461,14 +617,15 @@ void print_current_statistics(
     int moves_made,
     int apples_eaten,
     int apples_remaining,
-    int apples_initial
+    int apples_initial,
+    int points_remaining_total
 ) {
     double completion_percentage = 100.0;
     if (apples_initial != 0) {
         completion_percentage = 100.0 * apples_eaten / (double)apples_initial;
     }
 
-    int maximum_points_remaining = apples_remaining * 5;
+    int maximum_points_remaining = points_remaining_total;
 
     print_game_statistics(
         points,
@@ -478,6 +635,48 @@ void print_current_statistics(
         completion_percentage,
         maximum_points_remaining
     );
+}
+
+void reverse_snake(
+    int snake_rows[MAX_SNAKE_LENGTH],
+    int snake_cols[MAX_SNAKE_LENGTH],
+    int snake_length
+) {
+    for (int index = 0; index < snake_length / 2; index++) {
+        int opposite = snake_length - 1 - index;
+        int temp_row = snake_rows[index];
+        int temp_col = snake_cols[index];
+        snake_rows[index] = snake_rows[opposite];
+        snake_cols[index] = snake_cols[opposite];
+        snake_rows[opposite] = temp_row;
+        snake_cols[opposite] = temp_col;
+    }
+}
+
+void remove_tail_segments(
+    struct tile board[ROWS][COLS],
+    int snake_rows[MAX_SNAKE_LENGTH],
+    int snake_cols[MAX_SNAKE_LENGTH],
+    int *snake_length,
+    int segments_to_remove
+) {
+    if (segments_to_remove <= 0 || segments_to_remove >= *snake_length) {
+        return;
+    }
+
+    for (int index = 0; index < segments_to_remove; index++) {
+        int row = snake_rows[index];
+        int col = snake_cols[index];
+        board[row][col].entity = EMPTY;
+    }
+
+    int new_length = *snake_length - segments_to_remove;
+    for (int index = 0; index < new_length; index++) {
+        snake_rows[index] = snake_rows[index + segments_to_remove];
+        snake_cols[index] = snake_cols[index + segments_to_remove];
+    }
+
+    *snake_length = new_length;
 }
 
 
